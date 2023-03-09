71b3cc8e4795411616dc1827bcb75b7b17f32640
nome <email@test.com>
2023-03-08 22:31:21 -0300

    terminamos o projeto message

---
+++ app/src/main/java/com/ds2/sendmessages/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ds2.sendmessages
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view : View){
+        val textMessage = findViewById<EditText> (R.id.TextMessage)
+        //val displayMessage = findViewById<TextView>(R.id.displayMessage)
+        //displayMessage.text = textMessage.text
+        val message = textMessage.text.toString()
+        val intent = Intent (this, DisplayMessageActivity::class.java).apply{putExtra(EXTRA_MESSAGE, message) }
+        startActivity(intent)
+    }
+}
